41d4a84398445455b2517f8eaae3d2fdff781d7c
gabs1311 <gabs.antonovici@gmail.com>
2022-06-06 20:23:51 +0300

    day50

--- src/day50_Collections/ListSetPractice.java
+++ src/day50_Collections/ListSetPractice.java
@@ -16,8 +16,26 @@ public class ListSetPractice {
         System.out.println(list);
 
 
-        List<String> names = null;
-        System.out.println(names.size());
+     //   List<String> names = null;
+      //  System.out.println(names.size());
+
+        System.out.println("---------------------------------------------");
+
+        //pop()
+        List<Character>chars = new Stack<>();
+        chars.addAll(Arrays.asList('A','B','C', 'D'));// [A,B,C,D]
+        ((Stack) chars).pop();// it will shorten the array with one, applying FIFO rule
+        System.out.println("chars = " + chars);//[A,B,C]
+
+        // poll()==> FIFO
+
+        List<String>names =new LinkedList<>();
+        names.addAll(Arrays.asList("James", "Jimmy", "Kathy", "Breanna", "Max"));
+        System.out.println(names);// all elements will be printed
+        ((LinkedList<String>) names).poll();
+        System.out.println(names);// the first element will be removed
+
+
 
 
     }
