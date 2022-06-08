f76d68ab0a5762997ac76c1d6485b85dba6a26d7
gabs1311 <gabs.antonovici@gmail.com>
2022-06-08 18:27:05 +0300

    day50- RemovePalindromes

---
+++ src/day50_Collections/RemovePalindromes.java
@@ -0,0 +1,23 @@
+package day50_Collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemovePalindromes {
+    public static void main(String[] args) {
+
+        List<String>names = new ArrayList<>();
+        names.addAll(Arrays.asList("Java", "Python", "Cydeo", "Car", "Level", "civic", "radar", "kayak", "reviver", "racecar", "madam"));
+
+
+
+
+
+
+
+
+
+
+    }
+}
