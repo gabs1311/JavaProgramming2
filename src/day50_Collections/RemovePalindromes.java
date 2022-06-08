37675b70c63dae7d44f60d57123f8c95de73b9d9
gabs1311 <gabs.antonovici@gmail.com>
2022-06-08 19:14:46 +0300

    day50- RemovePalindromes

--- src/day50_Collections/RemovePalindromes.java
+++ src/day50_Collections/RemovePalindromes.java
@@ -2,6 +2,7 @@ package day50_Collections;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class RemovePalindromes {
@@ -11,10 +12,24 @@ public class RemovePalindromes {
         names.addAll(Arrays.asList("Java", "Python", "Cydeo", "Car", "Level", "civic", "radar", "kayak", "reviver", "racecar", "madam"));
 
 
+        Iterator<String>it = names.iterator();
+
+        while (it.hasNext()){
+            String each= it.next();
+            String reverse = "";
+            for (int i = each.length()-1; i <0 ; i--) {
+              reverse+=each.charAt(i);
+
+            }
+            if (each.equalsIgnoreCase(reverse)){
+                it.remove();
+            }
+        }
+
+        System.out.println(names);
 
 
-
-
+        System.out.println("-------------------------------");
 
 
 
