d8ea56c26c5d8960404d27c4e7fb7264eeb7d77e
gabs1311 <gabs.antonovici@gmail.com>
2022-06-11 17:29:19 +0300

    day51- MapIntro

--- src/day51_Map/IteratingTheMap.java
+++ src/day51_Map/IteratingTheMap.java
@@ -1,8 +1,6 @@
 package day51_Map;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class IteratingTheMap {
     public static void main(String[] args) {
@@ -17,22 +15,93 @@ public class IteratingTheMap {
         students.put("Serkan",70);
         students.put("Andriy",98);
 
+
         Set<String >keys = students.keySet();
     /*    for (String eachKey : keys) {
          //   System.out.println("eachKey = " + eachKey);
             students.replace(eachKey,students.get(eachKey)+5);
       */
-        // remove all the students whose score is less than 90
-        for (String eachKey : students.keySet()) {                // iterate each key of the map
-        Integer eachValue=students.get(eachKey);
-        if (eachValue<90){
-            System.out.println(eachKey);
+
+        System.out.println(students);
+        Map<String, Integer> earlyBirds = new HashMap<>(); // score >= 90
+        Map<String, Integer> angryBirds = new HashMap<>(); // score < 90
+
+
+        for (String key : students.keySet()) {      // I apply for each loop to get the key
+            Integer value = students.get(key);
+            if (value >= 90) {
+                earlyBirds.put(key, value);
+            } else {
+                angryBirds.put(key, value);
+            }
+
+        }
+        System.out.println("earlyBirds = " + earlyBirds);
+        System.out.println("angryBirds = " + angryBirds);
+
+        Set<String> names = students.keySet();
+        System.out.println("================================================");
+        //Collection<Integer> scores = students.values();
+        List<Integer> scores = new ArrayList<>(students.values());
+        System.out.println(scores);
+        for (Integer value : students.values()) {
+            System.out.println(value);
+        }
+
+        System.out.println("=============================================================");
+        int maxScore=Integer.MIN_VALUE;
+        int minScore=Integer.MAX_VALUE;
+        for (Integer score : students.values()) {
+            if (score>maxScore){
+                maxScore=score;
+            }
+            if (score<minScore){
+                minScore = score;
+            }
+        }
+        System.out.println("maxScore = " + maxScore);
+        System.out.println("minScore = " + minScore);
+
+        System.out.println("====================================");
+        int max = Collections.max(students.values());
+        int min= Collections.min(students.values());
+        System.out.println("max = " + max);
+        System.out.println("min = " + min);
+
+
+        // how many students has the score of 95 or greater
+         int count = 0;
+
+      /* // this is one way to solve it- the best
+        for (Integer eachScore : students.values()) {
+            if (eachScore>=95){
+                count++;
+
+            }
+        }
+        */
+        // another way, using keys, but the other one is  better, because if has one less step
+
+        for (String eachKey : students.keySet()) {
+          Integer eachScore =students.get(eachKey);
+            if (eachScore>=95){
+                count++;
+
+            }
         }
 
 
+
+        System.out.println(count);
+
+
+        System.out.println("=========================================");
+
+        for (Map.Entry<String, Integer> entry : students.entrySet()) {
+           // System.out.println(entry);
+            System.out.println(entry.getKey()+ " : "+ entry.getValue());
+
+            
         }
-
-
-
     }
 }
