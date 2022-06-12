ee4c95557f30e012cea2c547fc13e585aff1dbdd
gabs1311 <gabs.antonovici@gmail.com>
2022-06-12 09:43:42 +0300

    day51- Map Practice2

---
+++ src/day51_Map/MapPractice2.java
@@ -0,0 +1,31 @@
+package day51_Map;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapPractice2 {
+    public static void main(String[] args) {
+
+
+
+
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("John", 123000);
+        map.put("Antony", 100000);
+        map.put("Jimmy", 115000);
+        map.put("Jalil", 145000);
+        map.put("James", 110000);
+        map.put("Conor", 85000);
+        map.put("Josh", 117000);
+        map.put("Cory", 118000);
+        map.put("Anderson",125000);
+        map.put("Steven", 135000);
+
+        //  1 who has the maximum and minimum salary?
+
+        //how many employees has the salary between 120k ~ 150K?
+        //   1.4 display the names of the employees who are making less than 118k?
+        // 1.5 increase the salary employee by 10K if the current salary of employee is less than 120K
+
+        }
+}
