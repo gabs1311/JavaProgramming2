3bfcf527ae1c11ec1928e2c4ee944414ef3b4ba2
gabs1311 <gabs.antonovici@gmail.com>
2022-06-12 11:19:20 +0300

    day 51 - Map Practice 3

--- src/day51_Map/FrequencyOfCharacters.java
+++ src/day51_Map/FrequencyOfCharacters.java
@@ -1,7 +1,37 @@
 package day51_Map;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class FrequencyOfCharacters {
     public static void main(String[] args) {
+
+      /*  Write a program that can return the frequency of characters
+        Not: MUST use map
+        Ex: str = "bbcccaaaaa"
+        output:
+        {b=2, c=3, a=5}
+        */
+
+
+      String str = "bbcccaaaaa";
+        //           bca
+        //           235
+        //Collections.frequency()
+        
+      String []arr = str.split(" ");
+
+        Map<String, Integer> result = new LinkedHashMap<>();// if I use something else, I will not be
+        // getting this order   {b=2, c=3, a=5}
+
+        for (String each : arr) { // each: a,b,c  characters of string
+           int frequency =  Collections.frequency(Arrays.asList(arr), each);
+            result.put(each,frequency);
+        }
+
+        System.out.println(result);
 
     }
 }
