903f5ae7cf30faa6910f3ce986fb87f665d887a9
gabs1311 <gabs.antonovici@gmail.com>
2022-06-12 14:14:06 +0300

    day 51 -

---
+++ src/day52_Map_FunctionalInterface_Lambda/UniqueCharacters.java
@@ -0,0 +1,41 @@
+package day52_Map_FunctionalInterface_Lambda;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class UniqueCharacters {
+    public static void main(String[] args) {
+
+        /*
+1. Write a program that can find the unique characters from a String
+			Ex:
+					str = "aabcccdeeeef";
+			output:
+					{b=1, d=1, f=1}
+ */
+
+        String str = "aabcccdeeeef";
+
+        String []arr = str.split("");
+
+        Map<String, Integer> result= new LinkedHashMap<>();
+        for (String each : arr) {
+            int frequency = Collections.frequency(Arrays.asList(arr),each );
+            if (frequency==1){
+                result.put(each, 1);// under these conditions, the frequency is always 1
+            }
+        }
+        
+
+
+
+
+
+
+
+
+
+    }
+}
